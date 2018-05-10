2dd7a96cedd2e6515bdb32e0ef5511eda297df2d
BlackFeathers <18555119462@163.com>
2018-05-10 16:32:02 +0800

    新建数据库

---
+++ app/src/main/java/com/example/li/wardrobe/MyDBHelper.java
@@ -0,0 +1,40 @@
+package com.example.li.wardrobe;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+
+    private static final String CREATE_WARDROBE = "CREATE TABLE WARDROBE("+
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            "First text,"+
+            "Second text,"+
+            "Price real,"+
+            "Color text,"+
+            "Brand text,"+
+            "Remark text,"+
+            "Time text,"+
+            "Image Blob)";
+
+    private Context mContext;
+
+    public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                      int version){
+        super(context,"wardrobe.dbdb",null,1);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_WARDROBE);
+        Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
